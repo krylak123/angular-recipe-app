1131a896be1cfc3ba8f7c51c56498f37f43eaf7a
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-03-29 16:09:47 +0200

    feat: create logout button
    Created Logout Button in Header component.

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public isVisible: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    if (localStorage.getItem('isLogged') === 'yes') {
+      this.isVisible = true;
+    }
   }
 
+  handleOnLogoutClick() {
+    localStorage.setItem('isLogged', 'no');
+    this.isVisible = false;
+    this.router.navigate(['login']);
+  }
 }
