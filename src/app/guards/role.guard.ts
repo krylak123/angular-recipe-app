144513dd04b47bc826c13be8933e9bab60075380
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-04-03 17:31:06 +0200

    refactor: change RoleGuard

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { map, tap, take } from 'rxjs/operators';
-import { Role } from '../enums/role.enum';
 import { UserService } from '../services/user.service';
 
 @Injectable({
@@ -12,31 +11,16 @@ export class RoleGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot) {
     const canActivatesRole = route.data['roles'] as string[];
-    const userRoleValues: string[] = Object.values(
-      JSON.parse(localStorage.getItem('isLogged') || '{}')
+
+    return this.userService.user$.pipe(
+      map((role) => canActivatesRole.includes(role!.role)),
+      tap((canActivate) => {
+        if (canActivate) {
+          return;
+        }
+
+        this.router.navigate(['panel', 'details', '1']);
+      })
     );
-
-    if (canActivatesRole[0] === userRoleValues[0]) {
-      return true;
-    } else {
-      this.router.navigate(['panel', 'details', '1']);
-      return false;
-    }
-
-    // return this.userService.user$.pipe(
-    //   map(({ role }) => canActivatesRole.includes(role)),
-    //   tap((canActivate) => {
-    //     console.log(canActivate);
-
-    //     if (canActivate) {
-    //       return;
-    //     }
-
-    //     this.router.navigate(['panel', 'details', '1']);
-    //   })
-    // );
-
-    // console.log('role guard', canActivatesRole, Object.values(userRole));
-    // return true;
   }
 }
